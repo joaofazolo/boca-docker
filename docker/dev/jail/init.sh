5314ba43d58c991980d85885c9bb9a261d1ad0a5
Rodrigo Laiola Guimarães <rlaiola@gmail.com>
2022-06-28 19:06:57 -0300

    Repo refactoring and VS Code integration

--- docker/dev/jail/init.sh
+++ docker/dev/jail/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #========================================================================
-# Copyright Universidade Federal do Espirito Santo (Ufes)
+# Copyright 2020-2022 Universidade Federal do Espirito Santo (Ufes)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
